0e8c5e439a55999eda7e273ae7cab535dccc2e18
Fikkung <study0001fix@gmail.com>
2024-01-25 21:23:33 +0700

    first commit

--- main.go
+++ main.go
@@ -47,5 +47,5 @@ func main() {
 	authorized.GET("/readall", UserController.ReadAll)
 	authorized.GET("/profile", UserController.Profile)
 	r.Run("localhost:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-
+	//asd//
 }
